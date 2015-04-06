aa431e4b898e84f1aa2b8edd3d86bed1f3614246
Masafumi Yokoyama <myokoym@gmail.com>
2015-04-06 23:16:05 +0900

    Create sendlist if not found

--- shogiteatime.sh
+++ shogiteatime.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-cd /home/my/code/bot/twitter/shogiteatime
+cd /home/myokoym/src/ruby/shogiteatime
+if [ ! -f sendlist.yaml ]; then
+  echo "---" >sendlist.yaml
+  echo "" >>sendlist.yaml
+fi
 /usr/local/bin/yapra -c shogiteatime.yaml
 /usr/local/bin/ruby shogiteatime.rb
-
